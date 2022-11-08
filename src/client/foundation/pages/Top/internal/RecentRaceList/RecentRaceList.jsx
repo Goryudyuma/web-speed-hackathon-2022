c7736593862bef4c96fc664627cb377e287ff0b5
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2022-11-09 03:05:13 +0900

    jpgのままじゃないとherokuのサイズオーバーになった

--- src/client/foundation/pages/Top/internal/RecentRaceList/RecentRaceList.jsx
+++ src/client/foundation/pages/Top/internal/RecentRaceList/RecentRaceList.jsx
@@ -92,7 +92,7 @@ const Item = ({ race }) => {
 
         <Stack.Item grow={0} shrink={0}>
           <Stack horizontal alignItems="center" gap={Space * 2}>
-            <img src={race.image.slice(0,-3)+"webp?height=100&width=100"} />
+            <img src={race.image+"?height=100&width=100"} />
             <RaceButton to={`/races/${race.id}/race-card`}>投票</RaceButton>
           </Stack>
         </Stack.Item>
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const CompressionPlugin = require("compression-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const nodeExternals = require("webpack-node-externals");
-const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
+//const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
 
 function abs(...args) {
   return path.join(__dirname, ...args);
@@ -60,7 +60,7 @@ module.exports = [
         patterns: [{ from: PUBLIC_ROOT, to: DIST_PUBLIC }],
       }),
       new CompressionPlugin(),
-      new ImageminWebpWebpackPlugin(),
+      //new ImageminWebpWebpackPlugin(),
     ],
     resolve: {
       extensions: [".js", ".jsx"],
